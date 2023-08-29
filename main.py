89aa92c167b52be986ba085fd15f4750129ec70c
Spillertb <Spillertb@users.noreply.github.com>
2023-08-28 20:18:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Camera:
     def capture_video(self, duration_seconds: int) -> None:
         self.stop()
 
-        config = self.camera.create_video_configuration(raw=self.sensor_modes[-1])
+        config = self.camera.create_video_configuration(main=self.sensor_modes[-1])
         self.camera.configure(config)
 
         encoder = H264Encoder(10000000)
