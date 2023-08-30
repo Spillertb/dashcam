ccf53a494787c13a2d5e78e57aadc2acdfb3bc28
Spillertb <Spillertb@users.noreply.github.com>
2023-08-29 23:28:08 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -42,7 +42,7 @@ startTime = time.time()
 frames = 100
 prev_time = time.time()
 for i in range(frames):
-    buffers, metadata = camera.capture_buffers()
+    buffers, metadata = camera.capture_buffers(["raw"])
 
     pil_image = camera.helpers.make_image(buffers[0], config["main"])
 
