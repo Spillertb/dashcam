5b64b4ed0f75de82b105833245fcfde3f290514c
Spillertb <Spillertb@users.noreply.github.com>
2023-08-29 22:21:11 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,13 @@
+import threading
 from picamera2 import Picamera2
 import time
 import cv2
 import numpy as np
+
+def save_image_async(img, output_path):
+    cv2.imwrite(output_path, img)
+    print(f"Image saved to {output_path}")
+
 
 # Configure camera for 2028x1520 mode
 camera = Picamera2()
@@ -19,6 +25,8 @@ camera.start()
 
 time.sleep(1)
 
+
+active_threads = []
 # Capture frames and calculate FPS
 startTime = time.time()
 frames = 500
@@ -28,8 +36,18 @@ for i in range(frames):
     
     img = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
 
-    output_path = "output_image.jpg"
-    cv2.imwrite(output_path, img)
+    output_path = f"output_image_{time.time()}.jpg"
+
+    save_thread = threading.Thread(target=save_image_async, args=(img, output_path))
+    save_thread.start()
+
+    active_threads.append(save_thread)
+
+    # Prune finished threads from the list
+    active_threads = [t for t in active_threads if t.is_alive()]
+
+    # Print the number of active threads
+    print(f"Active threads: {len(active_threads)}")
 
     curr_time = time.time()
     print("image", i, (curr_time-prev_time)*10)
