d3dee21b16d276c2b7f5c1f9f85ae0604aeccab6
Spillertb <Spillertb@users.noreply.github.com>
2023-08-29 21:31:18 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ import time
 camera = Picamera2()
 config = camera.create_preview_configuration({"size": (2028, 1520)}, raw = camera.sensor_modes[2])
 camera.configure(config)
-camera.set_controls({"FrameRate": 40})
+camera.set_controls({"FrameRate": 10})
 
 # Start camera
 camera.start()
@@ -13,6 +13,7 @@ time.sleep(1)
 
 # Capture 100 frames and calculate FPS
 startTime = time.time()
-for i in range(100):
+frames = 50
+for i in range(frames):
     camera.capture_array("raw")
-print(1 / (time.time() - startTime) * 100)
+print(1 / (time.time() - startTime) * frames)
