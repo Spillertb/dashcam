616c964be9ee0f1ed969f5e95c0eccbafd404879
Spillertb <Spillertb@users.noreply.github.com>
2023-08-30 22:09:53 -0400

    updates

---
+++ hardware/camera.py
@@ -0,0 +1,62 @@
+import numpy as np
+from picamera2 import Picamera2
+import time
+import cv2
+
+
+size = (2304, 1296)
+
+frame_rate = 30
+
+
+class Camera:
+    camera: Picamera2() = None
+
+    config = None
+
+    def __init__(self):
+        self.camera = Picamera2()
+        sensor_modes = self.camera.sensor_modes
+
+        self.config = self.camera.create_preview_configuration(
+            main={"size": size}, raw=sensor_modes[1]
+        )
+        self.camera.configure(self.__format__config)
+
+        self.camera.set_controls({"FrameRate": frame_rate})
+
+        self.camera.start()
+        time.sleep(1)
+
+    def capture(self, frames: int = 100, output_path: str = "output_video.mp4"):
+        # Initialize the video writer
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        out = cv2.VideoWriter(output_path, fourcc, frame_rate, size)
+
+        # Capture frames and calculate FPS
+        startTime = time.time()
+        prev_time = time.time()
+        for i in range(frames):
+            buffers, metadata = self.camera.capture_buffers(["raw"])
+
+            pil_image = self.cameracamera.helpers.make_image(
+                buffers[0], self.config["raw"]
+            )
+
+            cv2_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+
+            # output_path = f"test.jpg"
+            # cv2.imwrite(output_path, cv2_image)
+
+            out.write(cv2_image)
+
+            curr_time = time.time()
+            duration = curr_time - prev_time
+            prev_time = curr_time
+            print("image", i, round((duration) * 1000, 2), "ms")
+
+        out.release()
+
+        print("average fps", 1 / (time.time() - startTime) * frames)
+
+        return output_path
--- main.py
+++ main.py
@@ -2,64 +2,61 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 from libcamera import controls
+from hardware.camera import Camera
 import os
 import time
 
 
-class Camera:
-    def __init__(self, hdr=False, fast_focus=False) -> None:
-        self.hdr = hdr
-        self.fast_focus = fast_focus
+# class Camera:
+#     def __init__(self, hdr=False, fast_focus=False) -> None:
+#         self.hdr = hdr
+#         self.fast_focus = fast_focus
 
-        self.camera = Picamera2()
+#         self.camera = Picamera2()
 
-        self.sensor_modes = self.camera.sensor_modes
+#         self.sensor_modes = self.camera.sensor_modes
 
-        print(f"sensor modes: {self.sensor_modes}")
+#         print(f"sensor modes: {self.sensor_modes}")
 
-        if hdr:
-            os.system("v4l2-ctl --set-ctrl wide_dynamic_range=1 -d /dev/v4l-subdev0")
-            print("Setting HDR to ON")
+#         if hdr:
+#             os.system("v4l2-ctl --set-ctrl wide_dynamic_range=1 -d /dev/v4l-subdev0")
+#             print("Setting HDR to ON")
 
-        if fast_focus:
-            camera.set_controls(
-                {
-                    "AfMode": controls.AfModeEnum.Continuous,
-                    "AfSpeed": controls.AfSpeedEnum.Fast,
-                }
-            )
+#         if fast_focus:
+#             camera.set_controls(
+#                 {
+#                     "AfMode": controls.AfModeEnum.Continuous,
+#                     "AfSpeed": controls.AfSpeedEnum.Fast,
+#                 }
+#             )
 
-        # let the camera warmup and stabalize brightness
-        time.sleep(1)
+#         # let the camera warmup and stabalize brightness
+#         time.sleep(1)
 
-    def start(self) -> None:
-        self.camera.start()
+#     def start(self) -> None:
+#         self.camera.start()
 
-    def stop(self) -> None:
-        self.camera.stop()
-        if self.hdr:
-            # this must be run after stopping the camera
-            print("Setting HDR to OFF")
-            os.system("v4l2-ctl --set-ctrl wide_dynamic_range=0 -d /dev/v4l-subdev0")
+#     def stop(self) -> None:
+#         self.camera.stop()
+#         if self.hdr:
+#             # this must be run after stopping the camera
+#             print("Setting HDR to OFF")
+#             os.system("v4l2-ctl --set-ctrl wide_dynamic_range=0 -d /dev/v4l-subdev0")
 
-    def capture_video(self, duration_seconds: int) -> None:
-        # self.stop()
+#     def capture_video(self, duration_seconds: int) -> None:
+#         print("Stopping camera capture")
+#         self.stop()
 
-        # config = self.camera.create_video_configuration(main={"size": (4608, 2592)})#, "format": "SRGGB10_CSI2P"})
-        # self.camera.configure(config)
-        self.camera.video_configuration.size = (4608, 2592)
-        encoder = H264Encoder(10000000)
-        output = FfmpegOutput("test.mp4")
+#         encoder = H264Encoder(10000000)
+#         output = FfmpegOutput("test.mp4")
 
-        self.camera.start_recording(encoder, output)
-        time.sleep(duration_seconds)
-        self.camera.stop_recording()
+#         self.camera.start_recording(encoder, output)
+#         time.sleep(duration_seconds)
+#         self.camera.stop_recording()
 
 
 if __name__ == "__main__":
     camera = Camera(hdr=False, fast_focus=False)
     # start_camera(camera)
 
-    camera.capture_video(duration_seconds=5)
-
-    camera.stop()
+    output = camera.capture()
